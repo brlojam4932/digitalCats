2eb5e51c9a692b98abff3420914abadf41a28889
brlojam4932 <36285465+brlojam4932@users.noreply.github.com>
2021-07-07 10:16:12 -0700

    updated crypto kitties contract

--- contracts/cryptoCats.sol
+++ contracts/cryptoCats.sol
@@ -1,44 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "./IERC721.sol";
-import "./IERC721Receiver.sol";
-import "./extensions/IERC721Metadata.sol";
-import "../../utils/Address.sol";
-import "../../utils/Context.sol";
-import "../../utils/Strings.sol";
-import "../../utils/introspection/ERC165.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract KittyContract is Context, ERC165, IERC721, IERC721Metadata {
-    using Address for address;
-    using Strings for uint256;
+contract myCryptoCats is ERC721 {
 
-  mapping(uint256 => address) public kittyIndexToOwner;
-  // an interger or index to an address
-  mapping(address => uint256) ownershipTokenCount; // an address to a number, a count
-  mapping(address => uint256[]) ownerToCats; //an address to a number of cats in an array
-  mapping(uint256 => address) kittyIndexToApproved; // point to another address to approve of transfer a tokens on the behalf of the owner
+  constructor () ERC721( "myCryptoCats", "MCC" ) {
 
-  //operator approval: first owner allows second owner to spend on his behalf
+  }
 
-  // MY ADDR => OPPERATOR ADDR => TRUE/FALSE
-  // _oppratorApprovals[myAddr][operatorAddr] = true/false
+  event Birth(address owner, uint256 kittenId, uint256 mumId, uint256 dadId, uint256 genes);
 
-  mapping (address => mapping(address => bool) ) private _operatorApprovals;
+  uint256 public constant CERATION_LIMIT_GEN0 = 10; // max num of cats to be generated
 
-  // implement different functions to set approval, get approval and set approval for all and get approval for all
-  // ex. 
-  // _operatorApprovals[myAddr][bobsAddr] = true;
-  // _operatorApprovals[myAddr][aliceAddr] = false;
-  // _operatorApprovals[myAddr][joesAddr] = true;
+  uint256 public gen0Counter;
 
-uint256 public constant CERATION_LIMIT_GEN0 = 10; // max num of cats to be generated
-uint256 public gen0Counter;
+  //an address to a number of cats in an array
+  mapping(address => uint256[]) ownerToCats;
 
-string public CryptoCatsToken;
-string public CRTT;
-
-struct Kitty{
+  struct Kitty{
   uint64 birthTime;
   uint32 mumId;
   uint32 dadId;
@@ -48,15 +28,74 @@ struct Kitty{
 
 Kitty[] kitties;
 
- // Token name
-    string private _name;
+  function totalSupply() public view returns (uint256 total) {
+    return kitties.length;
+  }
 
-    // Token symbol
-    string private _symbol;
+  function getAllCatsFor(address owner) public view returns (uint[] memory cats) {
+    return  ownerToCats[owner];
+  }
 
+  function getKitty(uint256 tokenId) public view returns (uint256 birthTime, uint256 mumId, uint256 dadId, uint256 generation, uint256 genes ) {
+    Kitty storage returnKitty = kitties[tokenId];
+    return (uint256(returnKitty.birthTime), uint256(returnKitty.mumId), uint256(returnKitty.dadId), uint256(returnKitty.generation), uint256(returnKitty.genes) );
+  }
 
+  function getKittyFilip(uint256 _id) public view returns (
+    uint256 birthTime,
+    uint256 mumId,
+    uint256 dadId,
+    uint256 generation,
+    uint256 genes 
+    ) {
 
+    Kitty storage kitty = kitties[_id];
 
+    birthTime = uint256(kitty.birthTime);
+    mumId = uint256(kitty.mumId);
+    dadId = uint256(kitty.dadId);
+    generation = uint256(kitty.generation);
+    genes = uint256(kitty.genes);
+    }
 
+    function createKittyGen0(uint256 _genes) public returns(uint256) { // needs onlyOwner
+      require(gen0Counter <= CERATION_LIMIT_GEN0, "Gen 0 should be less than creation limit gen 0" );
+
+      gen0Counter++;
+
+      // mum, dad and generation is 0
+    // Gen0 have no owners; they are owned by the contract
+    return _createKitty(0,0,0, _genes, msg.sender); // msg.sender could also be -- address(this) - we are giving cats to owner
+    }
+
+  // create cats by generation and by breeding
+  // returns cat id
+  function _createKitty(
+    uint256 _mumId,
+    uint256 _dadId,
+    uint256 _generation, //1,2,3..etc
+    uint256 _genes, // recipient
+    address owner
+  ) private returns(uint256) {
+    Kitty memory newKitties = Kitty({
+      genes: _genes,
+      birthTime: uint64(block.timestamp),
+      mumId: uint32(_mumId),
+      dadId: uint32(_dadId),
+      generation: uint16(_generation)
+    });
+
+    kitties.push(newKitties); // returns the size of array - 1 for the first cat
+
+    uint256 newKittenId = kitties.length -1; // 0-1
+
+    emit Birth(owner, newKittenId, _mumId, _dadId, _genes); // birth of a cat from 0 (standard)
+
+    _transfer(address(0), owner, newKittenId);
+
+    return newKittenId; //returns 256 bit integer
+
+  }
+  
 
 }
