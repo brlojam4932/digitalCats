3c58d96dfbb5412eeef5ecfae89d1fba9717ee05
brlojam4932 <36285465+brlojam4932@users.noreply.github.com>
2021-07-07 14:02:16 -0700

    new file

--- migrations/2_cryptoCats_migrations.js
+++
@@ -1,5 +0,0 @@
-const myCryptoCats = artifacts.require("myCryptoCats");
-
-module.exports = function (deployer) {
-  deployer.deploy(myCryptoCats);
-};
--- truffle-config.js
+++ truffle-config.js
@@ -42,11 +42,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-     ganache: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-     },
+    // development: {
+    //  host: "127.0.0.1",     // Localhost (default: none)
+    //  port: 8545,            // Standard Ethereum port (default: none)
+    //  network_id: "*",       // Any network (default: none)
+    // },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -92,15 +92,5 @@ module.exports = {
       //  evmVersion: "byzantium"
       // }
     }
-  },
-
-  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
-  //
-  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
-  // those previously migrated contracts available in the .db directory, you will need to run the following:
-  // $ truffle migrate --reset --compile-all
-
-  db: {
-    enabled: false
   }
 };
