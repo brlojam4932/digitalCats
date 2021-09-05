d3c9bc315b2cd069f87639af7b8a393a8ddb9133
brlojam4932 <36285465+brlojam4932@users.noreply.github.com>
2021-09-05 07:06:37 -0700

    Update 2_cryptoCats_migration.js

--- migrations/2_cryptoCats_migration.js
+++ migrations/2_cryptoCats_migration.js
@@ -1,5 +1,10 @@
 const myCryptoCats = artifacts.require("myCryptoCats");
 
+"dependencies": {
+  "@openzeppelin/contracts": ">=4.3.1"
+}
+
 module.exports = function (deployer) {
   deployer.deploy(myCryptoCats);
+  
 };
